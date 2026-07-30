4db756d5ac3929b80b49f2fe7cdb257fc2b144cc
10f43ayeshashaikhcanossa2021-collab <https://github.com/10f43ayeshashaikhcanossa2021-collab/smart-queue.git>
2026-07-30 19:05:33 +0530

    Add lower half landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import "./styles/lower.css";
+
+import Activation from "./components/Activation";
+import Pricing from "./components/Pricing";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Navbar />
+     <Navbar />
       <Hero />
+      <Activation />
+      <Pricing />
+      <CTA />
+      <Footer />
+
     </>
   );
 }
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,24 @@
+
+
+function Pricing() {
+  return (
+    <section className="pricing">
+
+      <div className="price-card">
+
+        <div className="left">
+          <small>MOST POPULAR</small>
+          <h2>$79/mo</h2>
+        </div>
+
+        <button>
+          Start Your Free Trial
+        </button>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default Pricing;
